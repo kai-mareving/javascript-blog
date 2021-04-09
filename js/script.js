59b92801f81cc89c536fd4036a66cec634200c81
Kai Mareving <karska.joanna@gmail.com>
2021-04-09 03:05:07 +0200

    Revert minor changes-clean up.

--- js/script.js
+++ js/script.js
@@ -21,7 +21,6 @@ const titleClickHandler = function (event) {
 }
 
 const links = document.querySelectorAll('.titles a');
-
 for (let link of links) {
     link.addEventListener('click', titleClickHandler);
 }
