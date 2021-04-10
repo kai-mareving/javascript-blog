b5252871b5c82f54237d4087a80ca26539cd7a77
Kai Mareving <karska.joanna@gmail.com>
2021-04-10 15:20:32 +0200

    Add function generateTitleLinks v.0

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*** Call active article to main column ***/
 const titleClickHandler = function (event) {
     event.preventDefault();
     const clickedElement = this;
@@ -30,7 +31,36 @@ const titleClickHandler = function (event) {
     console.log('targetArticle: ',targetArticle);
 }
 
+/*** Generate title links ***/
+const articles = document.querySelectorAll('.posts article');
+const articleIdList = [];
+for (let article of articles) {
+    let articleId = article.getAttribute('id');
+    articleIdList.push(articleId);
+}
+
+const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+const generateTitleLinks = function () {
+    console.log('generateTitleLinks is working!' + '\n' + 'Our articles: ', articles);
+    console.log('Article IDs: ', articleIdList);
+    
+    /* delete the contents of the link list in the left column */
+    /* then for each article: */
+    /* read its id and write it to a constant, (getAttribute)*/
+    // const articleId = 
+    /* find an element with a title and save its contents to a constant, (querySelector on article) */
+    /* based on this info, create HTML code for link and save it to a constant, */
+    /* insert this HTML code into the link list in the left column. */
+}
+
+
+/*** Action! ***/
 const links = document.querySelectorAll('.titles a');
 for (let link of links) {
     link.addEventListener('click', titleClickHandler);
 }
+
+generateTitleLinks();
